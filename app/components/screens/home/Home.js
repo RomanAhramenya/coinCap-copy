7c2dfe519edaa0217638f7cbaf0b532d1f5f41a6
Roman <romanahramenya@gmail.com>
2022-07-02 18:59:03 +0300

    table

--- app/components/screens/home/Home.js
+++ app/components/screens/home/Home.js
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Layout } from '../../layout/Layout'
 
 import { Tablecoins } from '../../ui/tableCoins/Tablecoins';
-import {MainStatisticsHeader} from '../../ui/mainStatisticsHeader/mainStatisticsHeader'
+// import MainStatisticsHeader from '../../ui/mainStatisticsHeader/mainStatisticsHeader'
 
 export const Home = () => {
 	const isThemeDark = useSelector(state => state.settings.isThemeDark)
@@ -14,6 +14,7 @@ export const Home = () => {
 
   return (
     <Layout title='Reliable Cryptocurrency Prices and Market Capitalizations'>
+    sdfsdf
       {/* <MainStatisticsHeader currency={currency} isThemeDark={isThemeDark} language={language}/> */}
 	  <Tablecoins  currency={currency} isThemeDark={isThemeDark} language={language}/>
     </Layout>
